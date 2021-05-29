cf4c7b66c1d264328c2d8667ebe631163e5b3496
kananrahimli <kenanrehimli48@gmail.com>
2021-05-29 17:37:48 +0400

    Last_commit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,62 +4,58 @@
       <ul class=" list-unstyled row justify-content-around">
         <li class="col-md-3 my-5 text-center">
           <img src="../assets/Watch.svg" alt="" />
+          <p class="mt-3 font-weight-bold px-5">Establishment of cargo management system in a short time</p>
         </li>
         <li class="col-md-3 my-5 text-center">
           <img src="../assets/cubics.svg" alt="" />
+          <p class="mt-3 font-weight-bold px-5">
+            Easy to use on all platforms for the client: website, ios, android
+          </p>
         </li>
         <li class="col-md-3 my-5 text-center">
           <img src="../assets/person.svg" alt="" />
+          <p class="mt-3 font-weight-bold px-5">Constantly evolving system</p>
         </li>
       </ul>
     </div>
 
-    <div class="main-section"  id="features">
+    <div class="main-section" id="features">
       <ul class="row">
-          
-          <Users/>
-          
-          <Admins/>
+        <Users />
+
+        <Admins />
       </ul>
     </div>
   </div>
 </template>
 
 <script>
-import Users from './Users.vue'
-import Admins from './Admins.vue'
+import Users from "./Users.vue";
+import Admins from "./Admins.vue";
 export default {
-  components:{
+  components: {
     Users,
-    Admins
+    Admins,
   },
   data() {
-    return {
-      
-    };
+    return {};
   },
 };
 </script>
 
 <style scoped>
-
-
 .main {
   margin-top: 350px;
 }
 
-
 .main-section {
   margin-top: 150px;
 }
- 
-
 
 @media screen and (max-width: 768px) {
   .main {
     margin-top: 250px;
   }
-   
 }
 
 @media screen and (max-width: 425px) {
--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="partners mt-5 " id="support">
-    <ul class="row justify-content-center mx-5 my-5 p-5 list-unstyled">
-          <transition name="change" v-for="(partner,index) in partners" :key="index">
-           <li class="col-md-5 " v-if="currentIndex==index">
-            <img :src="require('../assets/image/'+partner)" alt="" class="img-fluid"/>
-          </li>
-          </transition>
-          
+    <ul
+      class="row justify-content-around align-item-center mx-5 my-5 p-5 list-unstyled"
+    >
+      <li
+        class="col-md-3 mt-md-4 "
+      >
+        <img
+          src="../assets/image/suretkargo.png"
+          alt=""
+          class="img-fluid"
+        />
+      </li>
+      <li
+        class="col-md-3 ml-md-5 pl-md-5 my-md-0 my-5"
+      >
+        <img
+          src="../assets/image/bringly.svg"
+          alt=""
+          class="img-fluid"
+        />
+      </li>
+      <li
+        class="col-md-3  mx-4"
+      >
+        <img
+          src="../assets/image/starex.png"
+          alt=""
+          class="img-fluid"
+        />
+      </li>
+      
     </ul>
     <button class="right">
       <i class="fas fa-arrow-right" @click="slideRight"></i>
@@ -15,33 +39,28 @@
       <i class="fas fa-arrow-left" @click="slideLeft"></i>
     </button>
   </div>
-
 </template>
 
 <script>
 export default {
   data() {
     return {
-      direction:'',
-      currentIndex:0,
-      partners:[
-        'suretkargo.png',
-        'bringly.svg',
-        'starex.png'
-      ]
+      direction: "",
+      currentIndex: 0,
+      partners: ["suretkargo.png", "bringly.svg", "starex.png"],
     };
   },
   methods: {
     slideLeft() {
-      this.currentIndex--
-      if(this.currentIndex<0){
-        this.currentIndex=this.partners.length-1
+      this.currentIndex--;
+      if (this.currentIndex < 0) {
+        this.currentIndex = this.partners.length - 1;
       }
     },
     slideRight() {
-     this.currentIndex++
-      if(this.currentIndex>this.partners.length-1){
-        this.currentIndex=0
+      this.currentIndex++;
+      if (this.currentIndex > this.partners.length - 1) {
+        this.currentIndex = 0;
       }
     },
   },
@@ -49,7 +68,9 @@ export default {
 </script>
 
 <style scoped>
-
+li:first-child {
+  padding-top: 20px;
+}
 
 .partners {
   position: relative;
@@ -65,7 +86,7 @@ export default {
   top: 45%;
   left: 5%;
 }
-.col-md-5 img{
+.col-md-5 img {
   margin-left: 50%;
   transform: translate(-50%);
 }
@@ -96,14 +117,9 @@ export default {
   }
 }
 
-
-
-
 @media screen and (max-width: 768px) {
   .partners::before {
-  
-  font-size: 20px;
-  
-}
+    font-size: 20px;
+  }
 }
 </style>
